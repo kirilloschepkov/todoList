8ab4cb36bdab96f6059d4551436e244c0ad6870c
Kirill <k890484878789@gmail.com>
2022-12-26 13:52:11 +0500

    changed styles

--- scripts/todo-app/view.js
+++ scripts/todo-app/view.js
@@ -1,6 +1,7 @@
 const createTitleBlock = (text) => {
 	const block = document.createElement('h1')
 	block.textContent = text
+	block.classList.add('mt-3', 'mb-3')
 	return block
 }
 
@@ -8,7 +9,7 @@ const createStorageBlock = (storage, onClickFunc) => {
 	const btn = document.createElement('button')
 
 	btn.textContent = `Перейти на ${storage === 'localStorage' ? 'серверное' : 'локальное'} хранилище`
-	btn.classList.add('btn-storage')
+	btn.classList.add('btn', 'btn-primary', 'mb-3')
 
 	btn.addEventListener('click', async function () {
 		await onClickFunc(storage === 'localStorage' ? 'api' : 'localStorage')
@@ -19,14 +20,30 @@ const createStorageBlock = (storage, onClickFunc) => {
 
 const createFormBlock = () => {
 	const form = document.createElement('form'),
+		wrapper = document.createElement('div'),
+		label = document.createElement('label'),
 		input = document.createElement('input'),
-		button = document.createElement('button')
+		btn = document.createElement('button')
 
-	input.placeholder = 'Введите...'
-	button.textContent = 'Добавить дело'
+	label.textContent = 'Введите запланированное дело'
+	input.type = 'text'
+	btn.textContent = 'Добавить дело'
+	btn.type = 'submit'
 
-	form.append(input, button)
+	wrapper.classList.add('mb-3')
+	label.classList.add('form-label')
+	input.classList.add('form-control', 'bg-body-tertiary')
+	btn.classList.add('btn', 'btn-primary')
+
+	wrapper.append(label, input)
+	form.append(wrapper, btn)
 	return { form, input }
+}
+
+const createListBlock = () => {
+	const list = document.createElement('ol')
+	list.classList.add('mt-3')
+	return list
 }
 
 const createItem = (data, { onDone, onDelete }) => {
@@ -39,13 +56,17 @@ const createItem = (data, { onDone, onDelete }) => {
 	btnDone.textContent = 'Готово'
 	btnDel.textContent = 'Удалить'
 
+	item.classList.add('fs-3', 'mb-2')
+	btnDone.classList.add('btn', 'btn-outline-primary', 'btn-sm', 'me-2')
+	btnDel.classList.add('btn', 'btn-outline-primary', 'btn-sm')	
+
 	if (data.status === 'completed')
-		item.classList.add('done')
+		item.classList.add('text-decoration-line-through')
 
 	btnDone.addEventListener('click', async () => {
 		const response = await onDone(data)
 		if (response)
-			item.classList.toggle('done', data.status === 'completed')
+			item.classList.toggle('text-decoration-line-through', data.status === 'completed')
 	})
 
 	btnDel.addEventListener('click', async () => {
@@ -75,21 +96,21 @@ export default async ({
 	const titleBlock = createTitleBlock(title),
 		storageBlock = createStorageBlock(storage, onClickChangeStorage),
 		formBlock = createFormBlock(),
-		listBlock = document.createElement('ol'),
+		listBlock = createListBlock(),
 		handlers = { onDone: onClickDone, onDelete: onClickDelete }
 
 	listItems.forEach(e => {
 		listBlock.append(createItem(e, handlers))
 	});
-	
+
 	formBlock.form.addEventListener('submit', async e => {
 		e.preventDefault()
-		
+
 		const response = await onSubmitCreateItem({
 			owner,
 			title: formBlock.input.value.trim()
 		});
-		
+
 		if (response)
 			listBlock.append(createItem(response, handlers))
 		formBlock.input.value = ''
